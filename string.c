1c416e325b53da58626292783b516f4794b981b2
Yeonghoo_Ahn <77203153+AhnYeonghoo@users.noreply.github.com>
2021-06-22 19:11:35 +0900

    기말고사 입력한 문자열 중에서 뒤에 입력한 문자 찾는 문제

---
+++ string.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char str[1000], str2[1000], * p;
+
+	while (1)
+	{
+		printf("Enter two strings>> ");
+		scanf("%s", str);
+		if (strcmp(str, "quit") == 0)break;
+		scanf("%s", str2);
+		int sum = 0;
+		if (strcmp(str, "quit") == 0)break;
+		p = str;
+		while (1)
+		{
+			if (strcmp(str, "quit") == 0)break;
+			p = strstr(p, str2);
+			if (p != NULL) {
+				sum++;
+				p += strlen(str2);
+			}
+			else {
+				break;
+
+			}
+			printf("  %d\n\n\n", sum);
+
+		}
+	}
